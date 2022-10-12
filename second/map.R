f19f69a52ddde407ee4cea1d31d3bb94848afa42
vladimir <1287100643@qq.com>
2022-10-12 20:24:50 +0800

    success 2.0

--- second/map.R
+++ second/map.R
@@ -49,7 +49,7 @@ CHINA <- dplyr::left_join(China,SC_NEW,by= "QUHUADAIMA")
 p<-ggplot()+
   # 绘制主图
   geom_sf(data = CHINA,aes(fill = 性别比)) +
-  scale_fill_gradient(low="white",high="red")+
+  scale_fill_gradient(low="#FFFFBFFF",high="#FF0000FF")+
 
   # 绘制国境线及十/九段线
   geom_sf(data = gjx)+
